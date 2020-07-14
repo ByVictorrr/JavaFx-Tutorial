fbe4dff69f24906e6623058de544a67ab97dbb71
Delaplaine <victor.delaplaine@hpe.com>
2020-07-14 16:57:01 -0700

    tree views

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,5 +1,6 @@
 package sample;
 
+import com.sun.source.tree.Tree;
 import javafx.application.Application;
 import javafx.application.Preloader;
 import javafx.collections.ObservableList;
@@ -16,7 +17,7 @@ import javafx.stage.Stage;
 
 public class Main extends Application{
     Stage window;
-    ListView<String> listView;
+    TreeView<String> tree;
    public static void main(String[] args) {
         launch(args); // method inside application class
     }
@@ -25,31 +26,44 @@ public class Main extends Application{
     public void start(Stage primaryStage) throws Exception{
        window = primaryStage;
        window.setTitle("ComboBox demo");
-       listView = new ListView<>();
-       Button button = new Button("Submit");
-       listView.getItems().addAll("IRON man", "Titanic", "Contact");
-       listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+       TreeItem<String> root, bucky, megan;
+       // root
+        root = new TreeItem<>();
+        root.setExpanded(true);
 
-       button.setOnAction(e->buttonClicked());
+        // bucky
+        bucky = makeBranch("Bucky", root);
+        makeBranch("thenewboston", bucky);
+        makeBranch("Youtube", bucky);
+
+        // megan
+        megan = makeBranch("Megan", root);
+        makeBranch("tiktok", megan);
+        makeBranch("Youtube", megan);
+
+        tree = new TreeView<>(root);
+        tree.setShowRoot(false);
+        tree.getSelectionModel().selectedItemProperty().addListener((v,o,n)->{
+            if (n!=null){
+                System.out.print(n);
+            }
+        });
 
 
         VBox layout = new VBox(10);
         layout.setPadding(new Insets(20,20,20,20));
-        layout.getChildren().addAll(listView,button);
+        layout.getChildren().addAll(tree);
         Scene scene = new Scene(layout, 300, 250);
 
         window.setScene(scene);
         window.show();
 
     }
-    private void buttonClicked(){
-       String message = "";
-       ObservableList<String> movies;
-       movies =listView.getSelectionModel().getSelectedItems();
-       for(String m: movies){
-           message+=m;
-           System.out.print(message);
-        }
+    private TreeItem<String> makeBranch(String child, TreeItem<String> parent){
+        TreeItem<String>i = new TreeItem<>(child);
+        i.setExpanded(true);
+        parent.getChildren().add(i);
+        return i;
     }
 
 
