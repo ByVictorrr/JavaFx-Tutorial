d048dd210fb01d271e6b0c2e18a5779bcd731ca8
Delaplaine <victor.delaplaine@hpe.com>
2020-07-15 14:35:23 -0700

    Initilzer - do something call initalize after loading the view

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,17 @@
 package sample;
 
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
-/* Behind the schemes of the .fxml file (breaks up view and logic) */
-public class Controller {
-    // Dont have to assign it if already in fxml
-    public Button button;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    public void handleButtonClick(){
-        System.out.println("Run some code the user doesnt see");
-        button.setText("Steop touching me!");
+/* Behind the schemes of the .fxml file (breaks up view and logic) */
+public class Controller implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("Loading user data...");
+
     }
 }
