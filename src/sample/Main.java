c43532c9f6e6c3b269cfe06b0251df98c97273c2
Delaplaine <victor.delaplaine@hpe.com>
2020-07-15 10:41:53 -0700

    table views

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,6 +3,8 @@ package sample;
 import com.sun.source.tree.Tree;
 import javafx.application.Application;
 import javafx.application.Preloader;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -11,13 +13,16 @@ import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 
 public class Main extends Application{
     Stage window;
-    TreeView<String> tree;
+    TableView<Product> table;
+    TextField nameInput, priceInput, quantityInput;
+
    public static void main(String[] args) {
         launch(args); // method inside application class
     }
@@ -26,44 +31,84 @@ public class Main extends Application{
     public void start(Stage primaryStage) throws Exception{
        window = primaryStage;
        window.setTitle("ComboBox demo");
-       TreeItem<String> root, bucky, megan;
-       // root
-        root = new TreeItem<>();
-        root.setExpanded(true);
 
-        // bucky
-        bucky = makeBranch("Bucky", root);
-        makeBranch("thenewboston", bucky);
-        makeBranch("Youtube", bucky);
+       // Name column
+        TableColumn<Product, String> nameColumn = new TableColumn<>("Name");
+        nameColumn.setMinWidth(200);
+        nameColumn.setCellValueFactory(new PropertyValueFactory<Product, String>("name")); // tie data together
 
-        // megan
-        megan = makeBranch("Megan", root);
-        makeBranch("tiktok", megan);
-        makeBranch("Youtube", megan);
+        // Price Column
+        TableColumn<Product, String> priceColumn = new TableColumn<>("Price");
+        priceColumn.setMinWidth(200);
+        priceColumn.setCellValueFactory(new PropertyValueFactory<Product, String>("price")); // tie data together
 
-        tree = new TreeView<>(root);
-        tree.setShowRoot(false);
-        tree.getSelectionModel().selectedItemProperty().addListener((v,o,n)->{
-            if (n!=null){
-                System.out.print(n);
-            }
-        });
+        // Price Column
+        TableColumn<Product, String> quantityColumn = new TableColumn<>("Quantity");
+        quantityColumn.setMinWidth(200);
+        quantityColumn.setCellValueFactory(new PropertyValueFactory<Product, String>("quantity")); // tie data together
+
+        // Name input
+        nameInput = new TextField();
+        nameInput.setPromptText("Name");
+        // Price input
+        priceInput = new TextField();
+        priceInput.setPromptText("price");
+        // Price input
+        quantityInput = new TextField();
+        quantityInput.setPromptText("quantity");
+
+        // Btn
+        Button addBtn = new Button("add");
+        Button delBtn = new Button("delete");
+        addBtn.setOnAction(e->addBTNClicked());
+        delBtn.setOnAction(e->delBTNClicked());
+
+        HBox hbox = new HBox();
+        hbox.setPadding(new Insets(10,10,10,10));
+        hbox.setSpacing(10);
+        hbox.getChildren().addAll(nameInput,priceInput,quantityInput,addBtn,delBtn);
+
+
+
+
+        table = new TableView<>();
+        table.setItems(getProduct());
+
+        table.getColumns().addAll(priceColumn, nameColumn, quantityColumn);
+
 
 
         VBox layout = new VBox(10);
-        layout.setPadding(new Insets(20,20,20,20));
-        layout.getChildren().addAll(tree);
-        Scene scene = new Scene(layout, 300, 250);
+        layout.getChildren().addAll(table, hbox);
+        Scene scene = new Scene(layout);
 
         window.setScene(scene);
         window.show();
 
     }
-    private TreeItem<String> makeBranch(String child, TreeItem<String> parent){
-        TreeItem<String>i = new TreeItem<>(child);
-        i.setExpanded(true);
-        parent.getChildren().add(i);
-        return i;
+    private void addBTNClicked(){
+       Product p=new Product(nameInput.getText(), Double.parseDouble(priceInput.getText()), Integer.parseInt(quantityInput.getText()));
+       table.getItems().add(p);
+       nameInput.clear();
+       priceInput.clear();
+       quantityInput.clear();
+    }
+    private void delBTNClicked(){
+      ObservableList<Product> prodSelected, allProducts;
+      allProducts = table.getItems();
+      prodSelected = table.getSelectionModel().getSelectedItems();
+
+      prodSelected.forEach(allProducts::remove);
+    }
+
+    // Get all of the products
+    public ObservableList<Product> getProduct(){
+        ObservableList<Product> p = FXCollections.observableArrayList();
+        p.add(new Product("Laptop", 859.00, 20));
+        p.add(new Product("Bouncy ball", 2.00, 20));
+        p.add(new Product("Toilet", 99.00, 20));
+        p.add(new Product("Corn", 1.40, 20));
+        return p;
     }
 
 
