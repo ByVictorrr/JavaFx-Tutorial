b3ae5c3b0139e4c5136be033a068dfb390288d47
Delaplaine <victor.delaplaine@hpe.com>
2020-07-15 14:13:32 -0700

    How to update a label with userInput by binding

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,12 +3,15 @@ import javafx.application.Application;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -26,27 +29,23 @@ public class Main extends Application {
         window = primaryStage;
         window.setTitle("thenewboston");
 
-        IntegerProperty x = new SimpleIntegerProperty(3);
-        IntegerProperty y = new SimpleIntegerProperty();
+        // input and labels
+        TextField userInput = new TextField();
+        userInput.setMaxWidth(200);
+        Label firstLabel = new Label("Welcome to the site ");
+        Label secondLabel = new Label();
 
-        // value of y is connected to x multiplied by 10
-        y.bind(x.multiply(10));
-        System.out.println("x: " + x.getValue());
-        System.out.println("y: " + y.getValue() + "\n");
 
-        x.setValue(9);
-        System.out.println("x: " + x.getValue());
-        System.out.println("y: " + y.getValue() + "\n");
-
-        /* Why are bindings so important ?
-        *  Can bind a button to the scene (so when resize it -button moves with it)
-        * */
+        HBox bottomText = new HBox(firstLabel, secondLabel);
+        bottomText.setAlignment(Pos.CENTER);
 
         button = new Button("Submit");
+        VBox vBox = new VBox(10, userInput, bottomText);
+        vBox.setAlignment(Pos.CENTER);
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
-        Scene scene = new Scene(layout, 300, 250);
+        secondLabel.textProperty().bind(userInput.textProperty());
+
+        Scene scene = new Scene(vBox, 300, 250);
         window.setScene(scene);
         window.show();
     }
